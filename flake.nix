830bc2dcd52cb42d0a571a989481601d28111d5c
Thomas Boerger <thomas@webhippie.de>
2024-07-24 09:30:08 +0200

    ci: add flake workflow and update flakes

--- flake.nix
+++ flake.nix
@@ -3,34 +3,85 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
-    utils = {
-      url = "github:numtide/flake-utils";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
     };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+        inputs.pre-commit-hooks-nix.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        imports = [
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+        ];
+
+        pre-commit = {
+          settings = {
+            hooks = {
+              nixpkgs-fmt = {
+                enable = true;
+              };
+              golangci-lint = {
+                enable = true;
+              };
+            };
+          };
         };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            chart-testing
-            go
-            helm-docs
-            kind
-            kubeconform
-            kubernetes-helm
-            yamllint
-          ];
+
+        devenv = {
+          shells = {
+            default = {
+              languages = {
+                go = {
+                  enable = true;
+                  package = pkgs.go_1_22;
+                };
+              };
+
+              packages = with pkgs; [
+                chart-testing
+                helm-docs
+                kind
+                kubeconform
+                kubernetes-helm
+                nixpkgs-fmt
+                yamllint
+              ];
+
+              env = {
+                CGO_ENABLED = "0";
+              };
+            };
+          };
         };
-      }
-    );
+      };
+    };
 }
